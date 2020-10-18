54900950cc15f50e74e0a55fc38b30b377c7a549
tobinlee <616041023@qq.com>
2020-10-18 15:07:26 +0800

    first commit

--- app/src/main/java/com/tobin/top/ui/home/HomeFragment.java
+++ app/src/main/java/com/tobin/top/ui/home/HomeFragment.java
@@ -1,7 +1,7 @@
 package com.tobin.top.ui.home;
 
-import android.content.Intent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -10,7 +10,7 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.tobin.top.R;
 import com.tobin.top.base.BaseLazyFragment;
-import com.tobin.top.ui.recipe.RecipeActivity;
+import com.tobin.top.ui.window.WindowUtils;
 
 public class HomeFragment extends BaseLazyFragment<HomeViewModel> implements View.OnClickListener {
     TextView textView;
@@ -23,6 +23,12 @@ public class HomeFragment extends BaseLazyFragment<HomeViewModel> implements Vie
     protected void initView(View view) {
 
         textView = view.findViewById(R.id.text_home);
+
+        Button button = view.findViewById(R.id.window_pop);
+        button.setOnClickListener(view1 -> {
+            WindowUtils.showPopupWindow(activity);
+
+        });
     }
 
     @Override
